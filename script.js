3422a6f816b76780ba96bb411175c72f12de62a8
mooose22 <124860772+mooose22@users.noreply.github.com>
2023-03-20 03:22:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city, country) {
+  const response = await fetch(
+    apiUrl + city + "," + country + `&appid=${apiKey}`
+  );
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML =
+      data.name + ", " + data.sys.country;
+    document.querySelector(".country").innerHTML = "";
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°F";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " mi/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    checkWeather(searchBox.value);
+  }
+});
